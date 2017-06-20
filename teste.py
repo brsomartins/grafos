5a5294f2bbacae4ed7f1423048cbf099cabb878e
Breno Martins <brsomartins@gmail.com>
2017-06-20 17:31:28 -0300

    Adicionado arquivo com chamadas para os métodos da classe Grafo, para fins de testes dos estudos de caso

---
+++ teste.py
@@ -0,0 +1,27 @@
+from grafo import Grafo
+
+# Lê o grafo do arquivo "grafo_6.txt" e imprime a representação do grafo
+# print(Grafo.le_grafo("grafo_6.txt"))
+
+# Gera o arquivo "saida.txt" contendo informações sobre o grafo lido
+# Grafo.le_grafo("grafo_6.txt").gera_arquivo()
+
+# Gera uma lista de adjacência a partir do grafo lido e imprime sua 
+# representação
+# print(Grafo.le_grafo("grafo_6.txt").representa_grafo("lista"))
+
+# Gera uma matriz de adjacência a partir do grafo lido e imprime sua
+# representação
+# print(Grafo.le_grafo("grafo_6.txt").representa_grafo("matriz"))
+
+# Gera o arquivo "saida.txt" contendo informações (árvore de busca e níveis) 
+# sobre a busca em largura realizada no grafo lido
+# Grafo.le_grafo("grafo_6.txt").busca_grafo("bfs", "A")
+
+# Gera o arquivo "saida.txt" contendo informações (árvore de busca e níveis) 
+# sobre a busca em profundidade realizada no grafo lido
+# Grafo.le_grafo("grafo_6.txt").busca_grafo("dfs")
+
+# Gera o arquivo "saida.txt" contendo informações sobre componentes conexas 
+# descobertas no grafo lido
+Grafo.le_grafo("grafo_8.txt").descobre_componentes_conexas()
